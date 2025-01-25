f7a83b45e26ccfe4c0da3ba4ce01b8561aef1ed8
Alelith <eacaymo@gmail.com>
2025-01-25 23:36:23 +0000

    fvdeverfdrcvdrwcfdrw

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialog", menuName = "Dialog")]
+public class Dialog : ScriptableObject
+{
+    [SerializeField] string[] dialog;
+    
+    public string this[int i] => dialog[i]; 
+}
