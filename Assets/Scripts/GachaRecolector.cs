afef0aeb16cd2e2aa4ad5fecdac091806cbdc4a7
Antonio J. Armenteros <99495041+eterd3v@users.noreply.github.com>
2025-01-25 16:36:49 +0100

    Recolector

---
+++ Assets/Scripts/GachaRecolector.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GachaRecolector : MonoBehaviour
+{
+
+    //[SerializeField]
+
+
+    [SerializeField]
+    Rigidbody2D recolector;
+
+    [SerializeField]
+    float multiplicador = 1.0f;
+
+    void Update() {
+
+        Vector2 pos = new Vector2(transform.position.x,transform.position.y);
+   
+        if (Input.GetKey(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            recolector.MovePosition(pos + Vector2.left*Time.deltaTime*multiplicador);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            recolector.MovePosition(pos + Vector2.right*Time.deltaTime*multiplicador);
+        }
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+    if (other.CompareTag("ObjetoGacha")) {
+    // 
+    }
+
+    }
+
+}
